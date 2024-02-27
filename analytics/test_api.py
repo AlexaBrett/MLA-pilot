ec92a206be8c3374200c03b80c58e6877f860f3b
Harini Ramesh <hariniramesh95@gmail.com>
2024-02-27 21:14:46 +0530

    Update test_api.py

--- analytics/test_api.py
+++ analytics/test_api.py
@@ -2,10 +2,20 @@
 import unittest
 from flask import json
 from app import app
+import mongomock
 
 class AnalyticsAPITestCase(unittest.TestCase):
     def setUp(self):
+        # Create a mongomock client 
+        self.mock_client = mongomock.MongoClient()
+        app.config["MONGO_URI"] = "mongomock://localhost"
+
+        # Patch the application's database client with the mock client
+        app.db_client = self.mock_client
+
+        # Continue with setting up a test client for Flask
         self.app = app.test_client()
+        #test_client used to simulate requests and test reponse without running server
         self.app.testing = True
 
     def test_root_endpoint(self):
@@ -22,14 +32,12 @@ class AnalyticsAPITestCase(unittest.TestCase):
         self.assertTrue('stats' in data)  # Check if 'stats' key exists in response
 
     def test_user_stats_endpoint(self):
-        # Replace 'testuser' with a valid username from your database
         response = self.app.get('/stats/testuser')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertTrue('stats' in data)
 
     def test_weekly_user_stats(self):
-        # Ensure these query parameters are correctly formatted and valid
         response = self.app.get('/stats/weekly/?user=testuser&start=2022-01-01&end=2022-01-07')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
